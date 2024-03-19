f91468177773346d0397994ffccde0579ba676a1
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2024-03-19 18:41:39 -0400

    working on parsing TLE file better

---
+++ EclipseStudy.py
@@ -0,0 +1,40 @@
+import skyfield
+from skyfield.api import load, wgs84
+import io
+from skyfield.iokit import parse_tle_file
+
+x = open("sltrack_iridium_perm_small.txt", "r")
+
+
+satellites = list(parse_tle_file(x.read()))
+print(satellites)   
+
+
+
+
+
+
+
+
+
+# # Load the JPL ephemeris DE421 (covers 1900-2050).
+# planets = load('de421.bsp')
+# earth, mars = planets['earth'], planets['mars']
+
+# # What's the position of Mars, viewed from Earth?
+# astrometric = earth.at(t).observe(mars)
+# ra, dec, distance = astrometric.radec()
+
+# # print(ra)
+# # print(dec)
+# # print(distance)
+
+# from skyfield.api import N, W, S, E, wgs84
+
+# Roanoke = earth + wgs84.latlon(33.868 * S, 151.2093 * W)
+# astrometric = Roanoke.at(t).observe(mars)
+# alt, az, d = astrometric.apparent().altaz()
+
+# print(alt)
+# print(az)
+
