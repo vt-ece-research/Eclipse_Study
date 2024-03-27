e31d7e9812a51ef0ff8db9177ec66a1503cf906a
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2024-03-27 08:01:48 -0400

    Added time to lat and long function

--- EclipseStudy.py
+++ EclipseStudy.py
@@ -1,16 +1,95 @@
 import skyfield
-from skyfield.api import load, wgs84
+from skyfield.api import load, wgs84, EarthSatellite
 import io
 from skyfield.iokit import parse_tle_file
+import datetime as dt
 
-x = open("sltrack_iridium_perm_small.txt", "r")
+def satelliteParser():
+    #files and opens the file
+    with open('sltrack_iridium_perm_small.txt', 'r') as file:
+        #reads all lines into a list
+        lines = file.readlines()  
+        satelliteList = []
 
-TLEstring = x.read()
-S = TLEstring.strip().split(',')
-print(TLEstring)
-print(S)
+
+        #skips the first line which is the data
+        #the range function allows to increment by 3 lines 
+        #and start at line 1 instead of 0 and 
+        #go to the end of the file
+        for i in range(1, len(lines)):
+            singleSatellite = []
+            #takes each of the lines and gives them a 
+            #corresponding variable
+            temp = lines[i].split(",")
+            name = temp[0].strip()
+            tle1 = temp[1].strip()
+            tle2 = temp[2].strip()
+            NORADid = tle1[2:7]
+            #loads the timescale using the 
+            #official Earth Rotation data
+            singleSatellite.append(name)
+            singleSatellite.append(NORADid)
+            singleSatellite.append(tle1)
+            singleSatellite.append(tle2)
+
+            satelliteList.append(singleSatellite)
+
+    return satelliteList
+templist = satelliteParser()
 
 
+def satelliteFinderID(ID):
+    for element in templist:
+        if(element[1] == ID):
+            return element
+        
+
+def latandlongAtTime(number,time):
+    # ts = load.timescale()
+    tempsatellite = satelliteFinderID(number)
+    #Gets the satellite data loaded from a tle
+    satellite = EarthSatellite(tempsatellite[2], tempsatellite[3], tempsatellite[0])
+
+    #Gets the current realtime timescale
+    # t = ts.from_datetime(time)
+    #Gets the satellite's position
+    geocentric = satellite.at(time)
+    #finds the latitude and longitude of the satellite
+    lat, lon = wgs84.latlon_of(geocentric)
+
+    #prints all the data
+    print('Name:', tempsatellite[0])
+    print('Latitude:', lat)
+    print('Longitude:', lon)
+
+def latandlongFunction(number):
+    ts = load.timescale()
+    tempsatellite = satelliteFinderID(number)
+    #Gets the satellite data loaded from a tle
+    satellite = EarthSatellite(tempsatellite[2], tempsatellite[3], tempsatellite[0])
+
+    #Gets the current realtime timescale
+    t = ts.now()
+    #Gets the satellite's position
+    geocentric = satellite.at(t)
+    #finds the latitude and longitude of the satellite
+    lat, lon = wgs84.latlon_of(geocentric)
+
+    #prints all the data
+    print('Name:', tempsatellite[0])
+    print('Latitude:', lat)
+    print('Longitude:', lon)
+
+
+
+ts = load.timescale()
+t1 = ts.now()
+testtime = dt.datetime.now(dt.timezone.utc) 
+t = ts.from_datetime(testtime) +1
+latandlongAtTime('25104',t1)
+
+latandlongAtTime('25104',t)
+latandlongFunction('25104')
 
 
 
